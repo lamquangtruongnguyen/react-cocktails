bf024b7349473cb0d493ad5d700ea0d72ee7e4fb
lamquangtruongnguyen <tnguyen10@my.hpu.edu>
2023-01-18 15:58:50 -1000

    initial release

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut repellendus
+        reiciendis cupiditate. Blanditiis laborum dicta pariatur sit fuga autem
+        facere ex quis at quibusdam quasi beatae delectus, odit libero ratione
+        sequi id eius molestias provident, deserunt accusantium veniam excepturi
+        illum?
+      </p>
+    </section>
+  );
+};
+
+export default About;
